dda580cfa4ddeacbb7987947838fb6771dd49c37
Stephenod01 <stepheno@alchemytechs.com>
2023-12-15 15:11:59 +0000

    Pushing changes to branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+/*
+  Stephen O'Donnell 
+  Important!! :  this app is used to help me gain a better understanding of React,
+  there will be a lot of Comments within the code to help explain what things are,
+  What they do. 
+  How I'd explain JSX is JavaScript meets HTML
+*/
 import './App.css';
 
 function App() {
+  /* Normal Javascript function as it does not return any JSX tags such as <h1>,<p>, etc...
+  const getName = () => {
+    return "Stephen"
+  } 
+  */
+
+  /*  This is a component as it uses JSX as you can see with the HTML like elements
+  Components must start with a Capital letter (Best practices)  */
+  const GetInfoComponent = (props) =>{
+    return (
+      <div>
+        <h3>{props.name}</h3>
+        <h3>{props.age}</h3>
+        <h3>{props.email}</h3>
+      </div>
+    )
+  };
+
+  /*  Props allows the values to be passed through when the component is called
+  with example <Job salary={2500}...  */
+  const Job = (props) =>{
+    return (
+    <div>
+      <h1>Job Listing</h1>
+      <h3>Salary : {props.Salary}</h3>
+      <h3>position : {props.position}</h3>
+      <h3>Salary : {props.company}</h3>
+    </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <GetInfoComponent name="Stephen" age={23} email="Stephenod30@gmail.com"/>
+      <GetInfoComponent name="Sian" age={25} email="Siansmith9850@gmail.com"/>
+      <br/>
+      <Job Salary={35000} position="Software Engineer" company="Alchemy"/>
+      <Job Salary={45000} position="Senior Developer" company="All State"/>
+      <Job Salary={27500} position="Bussiness Analyst" company="Optum"/>
     </div>
   );
 }
